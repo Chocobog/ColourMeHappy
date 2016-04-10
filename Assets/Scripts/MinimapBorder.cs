53a8bf7238ac6e5c43285c136c9746a011d94f41
Josh <josh.hurn@hotmail.com>
2016-04-10 22:34:36 +1000

    HUD + Navmesh
    Working on HUD (template)
    navmesh update

---
+++ Assets/Scripts/MinimapBorder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapBorder : MonoBehaviour {
+
+    public Texture aTexture;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        //find way to import perfect image that fits minimap
+    }
+}
